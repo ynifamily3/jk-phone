b7dd58c22b30975b4d7b196771d12c8d8f0a5399
Jonekeun Choi <ynifamily3@gmail.com>
2019-02-24 22:03:45 +0900

    작업 환경 구성 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import "./env";
 import express from "express";
-import logger from morgan;
+import logger from "morgan";
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(logger("dev"));
+
+const handleListening = () => 
+    console.log(`♥ Listening on http://localhost:${PORT}`)
+
+app.listen(PORT, handleListening);
